733fe57127fda424196643f89264fc27826ba012
palonsovazquez <pav.vigo@gmail.com>
2018-10-23 11:28:47 +0200

    terminado Boletin4_4

--- Boletin4_4/src/boletin4_4/CalculadoraPolboExtra.java
+++ Boletin4_4/src/boletin4_4/CalculadoraPolboExtra.java
@@ -10,37 +10,36 @@ package boletin4_4;
  * @author Pablo Alonso Vazquez <pav.vigo@gmail.com>
  */
 public class CalculadoraPolboExtra {
-    
 
-
-private static final Float POLBOPAX = 2f/3f; //2 kg de polbo para 3 persoas.
-private static final Float PATACAPAX = 1f/3f; // 1 kg de patacas para 3 persoas.
-private Float polbo = 0f;
-private Float patacas = 0f;
-private Float racAct;
+    private static final Float POLBOPAX = 2f / 3f; //2 kg de polbo para 3 persoas.
+    private static final Float PATACAPAX = 1f / 3f; // 1 kg de patacas para 3 persoas.
+    private Float polbo = 0f;
+    private Float patacas = 0f;
+    private Float racAct;
 
     public CalculadoraPolboExtra() {
         this.patacas = 0f;
         this.polbo = 0f;
-        
-    }
-private void actRaciones(){
-    float racionesPat= this.patacas * PATACAPAX, x;
-    float racionesPolb = this.polbo * POLBOPAX;
-    if (racionesPat <= racionesPolb){
-        x = racionesPat;}
-    else{
-    x = racionesPolb;
-    }
-    this.racAct = x;
-}
 
-    public CalculadoraPolboExtra(Float polv,Float pat) {
+    }
+
+    private void actRaciones() {
+        float racionesPat = this.patacas * PATACAPAX, x;
+        float racionesPolb = this.polbo * POLBOPAX;
+        if (racionesPat <= racionesPolb) {
+            x = racionesPat;
+        } else {
+            x = racionesPolb;
+        }
+        this.racAct = x;
+    }
+
+    public CalculadoraPolboExtra(Float polv, Float pat) {
         this.setPatacas(polv);
         this.setPolbo(pat);
-        
-        
+
     }
+
     public Float amosarPolbo() {
         return polbo;
     }
@@ -58,25 +57,20 @@ private void actRaciones(){
         this.patacas = Patacas;
         this.actRaciones();
     }
-    
-    public void engadirPolbo(int x){
-        this.setPolbo(this.amosarPolbo() + x) ;
-        
-    
+
+    public void engadirPolbo(int x) {
+        this.setPolbo(this.amosarPolbo() + x);
+
     }
-    
-    public void engadirPatacas(int x ){
-    this.setPatacas(this.amosarPatacas()+ x);
-    
-    
+
+    public void engadirPatacas(int x) {
+        this.setPatacas(this.amosarPatacas() + x);
+
     }
-    
-    public Float getRaciones(){
-   
-    return this.racAct;
+
+    public Float getRaciones() {
+
+        return this.racAct;
     }
 
 }
-
-    
-
